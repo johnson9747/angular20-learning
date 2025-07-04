b713b8674974b9db1b541d348d77c4e44ed5e79b
johnson9747 <johnson9747@gmail.com>
2025-07-04 09:58:36 +0530

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
